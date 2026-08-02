4cffe1561c17ac62747d4f77d0e782f340bed05c
Firoz <mdfiroz0630@gmail.com>
2026-08-02 15:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.71%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -2,22 +2,7 @@ class Solution {
 public:
     bool stoneGame(vector<int>& nums) {
 
-           int first = 0;
-     int second = 0;
-        for(int i =0; i<nums.size(); i = i+2)
-        {
-          first+= nums[i];
-        }
-         for(int i =nums.size()-2; i>0; i = i-2)
-         {
-            second+=nums[i];
-         }
-       cout<<second<<" "<<first;
-       if(first>=second)
-       return true;
-
-       else
-       return 0;
+      return true;
         
     }
 };
