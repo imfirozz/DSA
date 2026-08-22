8b3d6fcd9697d8db794f0c5523b8bf1b23cf8ad2
Firoz <mdfiroz0630@gmail.com>
2026-08-23 00:41:31 +0530

    Time: 11 ms (9.51%) | Memory: 22.2 MB (55.27%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    bool check(string& s, int start, int end)
+    {
+        while (start < end) {
+            if (s[start] != s[end])
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+
+        int start = 0;
+        int end = s.size() - 1;
+
+        while (start < end) {
+
+            if (s[start] != s[end]) {
+
+                // Remove left character
+                // OR remove right character
+                return check(s, start + 1, end) ||
+                       check(s, start, end - 1);
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
