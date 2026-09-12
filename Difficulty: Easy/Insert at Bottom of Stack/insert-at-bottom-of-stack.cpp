f19453f10ac88606999b5cbc5b6f5c5777bed577
Firoz <mdfiroz0630@gmail.com>
2026-09-12 14:36:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insert at Bottom of Stack/insert-at-bottom-of-stack.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    stack<int> insertAtBottom(stack<int> &st, int x) {
+        // code here
+        stack<int>s1;
+       
+        while(!st.empty())
+        {
+            s1.push(st.top());
+            st.pop();
+        }
+        st.push(x);
+        while(!s1.empty())
+        {
+            st.push(s1.top());
+            s1.pop();
+        }
+        return st;
+    }
+};
