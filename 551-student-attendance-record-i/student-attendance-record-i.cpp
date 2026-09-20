8f5a377e23393a609d4a0401224b4cc624343188
Firoz <mdfiroz0630@gmail.com>
2026-09-20 22:44:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (82.94%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int countA = 0;
+        int countL = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i] == 'L') {
+                countL++;
+
+                if(countL >= 3)
+                    return false;
+            }
+            else {
+                countL = 0;
+            }
+
+            if(s[i] == 'A') {
+                countA++;
+
+                if(countA >= 2)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
